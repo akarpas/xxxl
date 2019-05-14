1909e0a95f137acf01f3aca4f0aa1af4609a82eb
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-14 17:23:39 +0200

    chore: implement basic design for wishlist view and add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DefaultWishlists from './data/defaultWishlists.json';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [wishlists, setWishlists] = useState(DefaultWishlists)
+
+  const addWishlist = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>XXXL Wishlist</h1>
+        <h3>Your Wishlists:</h3>
+        <ul>
+          {wishlists.map(wishlist => <li key={wishlist.id}>{wishlist.name}</li>)}
+        </ul>
+        <form onSubmit={addWishlist}>
+          <input type="text" min="2" />
+          <button type="submit"> Add Wishlist </button>
+        </form>
       </header>
     </div>
   );
