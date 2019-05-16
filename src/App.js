e6c7c8faed44174855ff48d481392d253c99107c
Andreas Karpasitis <karpasitis@outlook.com>
2019-05-16 18:33:33 +0200

    chore: refactor to correctly call patch while posting and also ability to update

--- src/App.js
+++ src/App.js
@@ -1,58 +1,108 @@
 import React, { useState } from 'react';
 import DefaultWishlists from './data/defaultWishlists.json';
 import { apiPost, apiPatch } from './fake-api/api';
-import Wishlists from './Wishlists';
+
 import './App.css';
 
 
 const App = () => {
+  const [pendingId, setPendingId] = useState(-1);
   const [wishlistName, setWishlistName] = useState('');
   const [wishlists, setWishlists] = useState(DefaultWishlists);
   const [loading, setLoading] = useState(false);
 
+  // Handle input of new wishlist name
   const onChange = event => {
     setWishlistName(event.target.value);
   }
 
-  const addWishlist = async event => {
+  // Function to add the new wishlist to state with negative ID
+  const add = event => {
     event.preventDefault();
-    // Copy the wishlists object and avoid mutating the original wishlists
-    const newWishlists = [...wishlists];
-    // Check ig there is a pending request by checking the wishlist state for negative ids
-    const pendingRequest = wishlists.findIndex(wishlist => wishlist.id === -1) !== -1;
-    // Set loading to show on UI that server response is still pending
-    setLoading(true);
+    const newWishlists = Object.assign(wishlists);
+    const wishlist = { id: pendingId, name: wishlistName, products: [] };
+    newWishlists.push(wishlist);
+    setPendingId(pendingId - 1);
+    setWishlists(newWishlists);
+    setWishlistName('');
+    addOrUpdateWishlist(wishlist, true);
+  }
 
+  // PROBLEM (NAIVE SOLUTION)
+  // ================================================================
+  // const addOrUpdateWishlistWrong = async wishlist => {
+  //   if (wishlist.id < 0) {
+  //     setLoading(true);
+  //     const savedWishlist = await apiPost({...wishlist});
+  //     const index = wishlists.findIndex(w => w.id === pendingId);
+  //     const updatedWishlists = [...wishlists];
+  //     updatedWishlists[index].id = savedWishlist.id;
+  //     setWishlists(updatedWishlists);
+  //     setLoading(false);
+  //   } else {
+  //     console.warn('PATCH A SAVED')
+  //     await apiPatch(wishlist);
+  //   }
+  // }
+
+
+  // Function to add or update a wishlist by calling the API
+  const addOrUpdateWishlist = async (wishlist, newList) => {
     // We need to check if there is a pending request from the server
     // in the case there is a pending request, then do a patch instead of a post
-    if (pendingRequest) {
-      const index = newWishlists.findIndex(wishlist => wishlist.id === -1);
-      newWishlists[index].name = wishlistName;
-      setWishlists([...newWishlists]);
-      const data = await apiPatch([...newWishlists], wishlistName);
-      setWishlists(data);
+    if (wishlist.id < 0 && loading && !newList) {
+      console.log('PATCH A POST');
+      // NOTE: console.log kept on purpose to show at which cases it is triggered
+      await apiPatch(wishlist);
+    } else if (wishlist.id < 0) {
+      console.log('POST NEW');
+      // NOTE: console.log kept on purpose to show at which cases it is triggered
+
+      // Set loading to show on UI that server response is still pending
+      // Also to use to check whether someone is editing a wishlist that is still in post
+      setLoading(true);
+      const savedWishlist = await apiPost({...wishlist});
+      const index = wishlists.findIndex(w => w.id === pendingId);
+      const updatedWishlists = [...wishlists];
+      updatedWishlists[index].id = savedWishlist.id;
+      setPendingId(pendingId === -1 ? pendingId : pendingId + 1);
+      setWishlists(updatedWishlists);
       setLoading(false);
     } else {
-      newWishlists.push({ id: -1, name: wishlistName, products: [] })
-      setWishlists([...newWishlists]);
-      const data = await apiPost([...newWishlists]);
-      setWishlists(data);
-      setLoading(false);
+      console.log('PATCH A SAVED');
+      // NOTE: console.log kept on purpose to show at which cases it is triggered
+      await apiPatch(wishlist);
     }
   }
 
+  // Function to handle changing a wishlist name on the UI
+  const handleNameChange = async event => {
+    const { id, value } = event.target;
+    const updatedWishlists = [...wishlists];
+    const indexUpdatedWishlist = updatedWishlists.findIndex(wishlist => wishlist.id === Number(id));
+    updatedWishlists[indexUpdatedWishlist].name = value;
 
-  // TO DO: replace wishlist list with inputs so that you can call
-  // PATCH and update the name of a wishlist
+    setWishlists(updatedWishlists)
+    addOrUpdateWishlist({ id, name: value });
+  }
+  console.warn(wishlists);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>XXXL Wishlist</h1>
+        <h1>XXXL Wishlist Manager</h1>
         <h3>Your Wishlists:</h3>
-        <Wishlists wishlists={wishlists} />
-        {loading && <div>- Updating your Wishlists -</div>}
-        <form onSubmit={addWishlist}>
-          <input type="text" minLength="2" onChange={onChange} />
+        <ul>
+          {wishlists.map(wishlist => {
+            return (
+              <li key={wishlist.id}>
+                <input id={wishlist.id} value={wishlist.name} onChange={handleNameChange} />
+              </li>
+            )
+          })}
+        </ul>
+        {loading && <div>- Saving your Wishlists -</div>}
+        <form onSubmit={add}>
+          <input value={wishlistName} type="text" minLength="2" onChange={onChange} />
           <button type="submit"> Add Wishlist </button>
         </form>
       </header>
--- src/fake-api/api.js
+++ src/fake-api/api.js
@@ -1,17 +1,14 @@
-export const apiPost = async (wishlists) => {
+const defaultWishes = require('../data/defaultWishlists.json');
+let lastId = defaultWishes.length + 1;
+
+export const apiPost = async (wishlist) => {
   await delay(3000)
-  const index = wishlists.findIndex(wishlist => wishlist.id === -1);
-  const id = wishlists.length
-  wishlists[index].id = id;
-  return [...wishlists]
+  wishlist.id = lastId++;
+  return wishlist;
 }
 
-export const apiPatch = async (wishlists, wishListName) => {
-  await delay(3000)
-  const id = wishlists.length
-  const index = wishlists.findIndex(wishlist => wishlist.id === id);
-  wishlists[index].name = wishListName;
-  return [...wishlists];
+export const apiPatch = async (wishlist) => {
+  return wishlist;
 }
 
 const delay = ms => new Promise(res => setTimeout(res, ms))
